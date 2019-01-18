13789276cd7526e1aaba00d4991d97ea65f060ce
jeanjoulia <jean.joulia@isen.yncrea.fr>
2019-01-18 14:53:18 +0100

    User model is finished

--- models/user.js
+++ models/user.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Create a Schema and a Model
-
-const Tag = new Schema({
-    title: String,
-    weight: Number
-});
+// Create Schemas and a Model
 
 const UserSchema = new Schema({
+  email: { type: String, required: true, unique: true},
+  password: { type: String, required: true},
+  name: { type: String, required: true, unique: true},
+  host: [{ type : ObjectId, ref: 'host' }],
+  animator: [{ type : ObjectId, ref: 'animator' }],
+  Preference:[PreferenceSchema]
+});
+
+const PreferenceSchema = new Schema({
     name: String,
-    age:Number,
-    tag: [Tag]
+    weight: Number
 });
 
 const User = mongoose.model('user', UserSchema);
