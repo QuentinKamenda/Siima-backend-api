b35462202b256a29b6f4101f1be16efaa49b2c33
Quentin Kamenda <quentin.kamenda@isen.yncrea.fr>
2019-08-15 14:59:50 +0200

    Offer and Event Model Update
    Update Models (required fields and names)

--- models/event/event.js
+++ models/event/event.js
@@ -2,20 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
+
+  // Internal information
+  status: {type: String, default: 'created'},
+
+  // Descriptive information
   name: { type: String, required: true},
   tags: [{type: String, required: false}],
+  description: { type: String, required: true},
+  location: {type: String, required: true},
+  date: {type: String, required: true},
+  price: {type: Number, default: 0},
+  remainingPlaces: {type: Number, default: -1},
+
+  // Links with other entities
   participants: [{ type : Schema.Types.ObjectId, ref: 'User' }],
   animators: [{ type : Schema.Types.ObjectId, ref: 'Animator' }],
   hosts:[{type : Schema.Types.ObjectId, ref: 'Host'}],
   admins: [{ type : Schema.Types.ObjectId, ref: 'User' }],
-  description: { type: String},
+
+  // Media
   media:{type: Schema.Types.ObjectId , ref : 'Media'},
-  price: Number,
+  profile_picture:{type: Schema.Types.ObjectId, ref: 'Media'},
+
+  // Unimplemented
   links: [ {type:Schema.Types.ObjectId, ref:'Link'}],
   facilities: [{type: String}],
-  remainingPlaces: Number,
-  lieu: {type : String , required: true},
-  profile_picture:{type: Schema.Types.ObjectId, ref: 'Media'},
+  access: [{type: String}],
   startingDate : String,
   endingDate : String
 },{timestamps : true});
--- models/event/offer.js
+++ models/event/offer.js
@@ -2,20 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const offerSchema = new Schema({
+
+  // Internal information
+  status: {type: String, default: 'created'},
+
+  // Descriptive information
   name: { type: String, required: true},
   tags: [{type: String, required: false}],
-  participants: [{ type : Schema.Types.ObjectId, ref: 'User' }],
+  description: { type: String, required: true},
+  location: {type: String, required: true},
+  date: {type: String, required: true},
+
+  // Links with other entities
   animators: [{ type : Schema.Types.ObjectId, ref: 'Animator' }],
   hosts:[{type : Schema.Types.ObjectId, ref: 'Host'}],
   admins: [{ type : Schema.Types.ObjectId, ref: 'User' }],
-  description: { type: String},
+
+  // Media
   media:{type: Schema.Types.ObjectId , ref : 'Media'},
+  profile_picture:{type: Schema.Types.ObjectId, ref: 'Media'},
+
+  // Unimplemented
+  participants: [{ type : Schema.Types.ObjectId, ref: 'User' }],
   price: Number,
   links: [ {type:Schema.Types.ObjectId, ref:'Link'}],
   facilities: [{type: String}],
   remainingPlaces: Number,
-  lieu: {type : String , required: true},
-  profile_picture:{type: Schema.Types.ObjectId, ref: 'Media'},
   startingDate : String,
   endingDate : String
 },{timestamps : true});
